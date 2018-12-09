409ed524eff2dbdab7d30f771b3636bd41163caa
Benjamin Alt <benjamin_alt@outlook.com>
2018-12-09 12:46:55 +0100

    Fixed indentation

--- script.py
+++ script.py
@@ -22,43 +22,44 @@ else:
     training_set = np.load(train_archive)
     test_set = np.load(test_archive)
 
-training_labels = training_set[:, 0]
-training_data = training_set[:, 1:]
-training_data = training_data.reshape((-1, 28, 28))
-test_labels = test_set[:,0]
-test_data = test_set[:,1:]
-test_data = test_data.reshape((-1,28,28))
+#training_labels = training_set[:, 0]
+#training_data = training_set[:, 1:]
+#training_data = training_data.reshape((-1, 28, 28))
+#test_labels = test_set[:,0]
+#test_data = test_set[:,1:]
+#test_data = test_data.reshape((-1,28,28))
 
-Image.fromarray(training_data[0]).save("test.png")
+#Image.fromarray(training_data[0]).save("test.png")
 
-print("done")
+#print("done")
+
 
 class SimpleCNN(torch.nn.Module):
 
- def __init__(self):
-     super(SimpleCNN, self).__init__()
+    def __init__(self):
+        super(SimpleCNN, self).__init__()
 
-     self.conv1 = torch.nn.Conv2d(1, 24, kernel_size=3, stride=1, padding=1)
-     self.pool = torch.nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
-     self.fc1 = torch.nn.Linear(18 * 16 * 16, 64)
-     self.fc2 = torch.nn.Linear(64, 24)
+        self.conv1 = torch.nn.Conv2d(1, 18, kernel_size=3, stride=1, padding=1)
+        self.pool = torch.nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.fc1 = torch.nn.Linear(18 * 14 * 14, 64)
+        self.fc2 = torch.nn.Linear(64, 24)
 
- def forward(self, x):
-     x = F.relu(self.conv1(x))
-     x = self.pool(x)
-     x = x.view(-1, 18 * 16 *16)
-     x = F.relu(self.fc1(x))
-     x = self.fc2(x)
-     return(x)
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = x.view(-1, 18 * 14 *14)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
 
-n_training_samples = 20000
-train_sampler = SubsetRandomSampler(np.arange(n_training_samples, dtype=np.int64))
+n_training_samples = training_set.shape[0]
+train_sampler = SubsetRandomSampler(np.arange(n_training_samples, dtype=np.uint8))
 
 n_val_samples = 5000
-val_sampler = SubsetRandomSampler(np.arange(n_training_samples, n_training_samples + n_val_samples, dtype=np.int64))
+val_sampler = SubsetRandomSampler(np.arange(n_training_samples, n_training_samples + n_val_samples, dtype=np.uint8))
 
 n_test_samples = 5000
-test_sampler = SubsetRandomSampler(np.arange(n_test_samples, dtype=np.int64))
+test_sampler = SubsetRandomSampler(np.arange(n_test_samples, dtype=np.uint8))
 
 def get_train_loader(batch_size):
     train_loader = torch.utils.data.DataLoader(training_data, batch_size=batch_size,
@@ -126,8 +127,8 @@ def trainNet(net, batch_size, n_epochs, learning_rate):
             print("Training finished, took {:.2f}s".format(time.time() - training_start_time))
 
 def main():
- CNN = SimpleCNN()
- trainNet(CNN, batch_size=32, n_epochs=5, learning_rate=0.001)
+    CNN = SimpleCNN()
+    trainNet(CNN, batch_size=32, n_epochs=5, learning_rate=0.001)
 
 if __name__ == "__main__":
- main()
+    main()
